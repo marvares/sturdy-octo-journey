b23eca9927b782bba30641a5722c18a709a37f6b
Francisco Martín Vargas <francisco.vargas@pucp.pe>
2026-06-02 22:31:27 +0000

    Update de los pasos iniciales y finales, edición CSR00, desde nueva instancia Posit

--- Final Step.R
+++ Final Step.R
@@ -1,5 +1,3 @@
-# Cuando recién cloneo:
-blogdown::install_hugo(version = "0.72.0", force = TRUE)
 #  Cuando hago mis cambios:
 blogdown::serve_site()
 # Para construirlo:
--- First Step.R
+++ First Step.R
@@ -1,1 +1,17 @@
-install.packages(c("blogdown", "papaja", "gt", "markdown", "psych", "corrplot", "tidytext"))
+# Para empezar
+install.packages(c("blogdown", "papaja", "gt", "rmarkdown", "tidyverse", "usethis", "gitcreds"))
+
+# Se supone que ya clonée el repo "sturdy-octo-journey" para crear el proyecto. Debo fijarme de que se trate de la rama "perusicosocial", y NO de la rama "master"
+
+# Después de clonar
+usethis::use_git_config(user.name = "Francisco Martín Vargas", user.email = "francisco.vargas@pucp.pe")
+
+# Antes de poder trabajar hay que instalar el tema "Hugo":
+blogdown::install_hugo(version = "0.72.0", force = TRUE)
+
+# luego darle el PAT para sincronizar los cambios en el repo:
+gitcreds::gitcreds_set()
+
+# El PAT para github está en Google Keep y es permanente (de ser necesario se puede crear uno nuevo)
+# Se recomienda usar el editor visual para escribir
+
